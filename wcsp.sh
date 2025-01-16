6263de6ad4fa19716c131f257ef46b676d025203
Yony Marian <yonyon.marian@gmail.com>
2025-01-16 00:02:09 -0800

    implement WinSCP functionality

--- wcsp.sh
+++ wcsp.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# sources constants
-function build_ip() {
-
-
 
 # Opens WinSCP and transfers files
-function open_winscp() {
+function transfer_to_winscp() { # <username> <password> <directory_name>
 	if [[ "$#" -ne 3 ]]; then
 		echo "Bad arguments passed to open_winscp()"
 		return
-	local hostname = "$1"
-	local username = "$2"
-	local password = "$3"
+	local username="$1"
+	local password="$2"
+	local dirname="$3"
+	local hostname="${CLASS}.${URL_TAIL}" # from env_vars.txt
+	local remote_path="${REMOTE_PATH_PREFIX}" # from env_vars.txt
+	local sftp_connection="$username:$password@$hostname"
+
+	# create WinSCP temp script to open with
+	local winscp_script=$(mktemp)
+	echo "open sftp://${connection}" >> "${winscp_script}"
+	echo "option batch on" >> "${winscp_script}"
+	echo "option confirm off" >> "${winscp_script}"
+	echo "synchronize remote ./${dir_name} $remote_path/${dir_name}" >> "${winscp_script}"
+	echo "exit" >> "${winscp_script}"
+
+	# execute the script in WinSCP
+	echo "Opening WinSCP and syncing files..."
+	winscp.com /script="$winscp_script"
+	echo "The remote directory is now up-to-date."
+	rm -f "$winscp_script"
 }
 
 # Main logic
@@ -20,6 +33,11 @@ function main() {
 	# check for correct args
 	if [[ "$#" -ne 1 ]]; then
 		echo "usage: wcsp <directory_name>"
+		return
+	fi
+	local dir_name="$1"
+	if [[ ! -d "$1" ]]; then
+		echo -e "Could not find directory: \'${1}\'"
 		return
 	fi
 	# prompt for username
@@ -33,4 +51,9 @@ function main() {
 	read -p -s "Enter password: " password
 	if [[ ! "$password" ]]; then
 		echo "You must enter a password."
+	# source constants, if found
+	source env_vars.txt || return
+	echo "Using constants in env_vars.txt."
+	echo "Preparing to open WinSCP..."
+	transfer_to_winscp "$username" "$password" "$dir_name"
 }
