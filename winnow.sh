c2895bad9d93341f87c77e64670f4ba2fcfc4f10
Yony Marian <yonyon.marian@gmail.com>
2025-02-08 14:21:02 -0800

    bug fixes

--- winnow.sh
+++ winnow.sh
@@ -6,6 +6,7 @@ readonly USAGE_GUIDELINES="Usage: winnow <source> <file extension(s)> <target>"
 # automatically copy desired file types to specified folder
 # Usage: winnow <source> <file extensions>... <destination>
 function winnow() {
+
 	# must be at least one source, one file ext, one target
 	if [[ "$#" -lt 3 ]]; then
 		echo "${USAGE_GUIDELINES}"
